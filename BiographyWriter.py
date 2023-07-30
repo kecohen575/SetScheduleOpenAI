d77bb91b8010230d06aad728e10a54651616fbae
kecohen575 <103701670+kecohen575@users.noreply.github.com>
2023-07-29 23:02:38 -0700

    Add files via upload

---
+++ BiographyWriter.py
@@ -0,0 +1,56 @@
+import openai
+
+K_API_KEY = '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'
+S_API_KEY = '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'
+openai.api_key = S_API_KEY
+
+# getting key/personal info
+expertise = input("\nWhat is your area of expertise?\n")
+achievements = input("\nAny notable achievments/awards?\n")
+philosophy = input("\nWhat is your professional philosophy?\n")
+interests = input("\nWhat are your personal interests/hobbies?\n")
+trait = input("\nWhat character trait do you want to shine through most in this bio?\n")
+
+# check for trait sentiment
+traitPositive = False
+while traitPositive == False:
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = f"Answer either \"positive\", \"negative\", or \"neutral. Determine the sentiment of the character trait: {trait}",
+        temperature = 0,
+        max_tokens = 25,
+    )
+    if "negative" in response["choices"][0]["text"].lower():
+        trait = input("\nPlease use a more positive character trait.\n")
+    else:
+        traitPositive = True
+
+# POTENTIAL ISSUE:
+# Rate limit for ai calls is 3/min. If user inputs two negative traits in a row, the third input (even if positive) will cause program to end.
+# When integrated into app, is rate limit individual to each user, or does the app as a whole follow strict 3/min?
+
+johnPrompt = f"""
+Write a biography (around 200 words, one paragraph) for John Doe, a realtor in Los Angeles, based off this information:
+Works for: SetSchedule
+5 year(s) of experience
+Area of expertise: {expertise}
+Notable achievements/awards: {achievements}
+Professional philosophy: {philosophy}
+Personal interest(s): {interests}
+John wants their {trait} to shine through in their bio.
+"""
+
+response = openai.Completion.create(
+    model = "text-davinci-003",
+    prompt = johnPrompt,
+    max_tokens = 250,
+    temperature = 1,
+    frequency_penalty = 1,
+)
+
+# POTENTIAL ISSUE:
+# davinci model being used will be retired beginning of 2024
+
+print("\nHere's your personalized biography:")
+biography = response["choices"][0]["text"][1:]
+print(biography)
